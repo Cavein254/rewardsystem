75f9c68fd7a8ac2ae9a037d7f2792d79b72ac364
johnmuthua <caveomuthama@gmail.com>
2024-06-05 13:46:55 +0300

    Add apollo client

---
+++ client/graphql/apollo-client.ts
@@ -0,0 +1,18 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+
+if (process.env.NODE_ENV !== "production") {
+  // Adds messages only in a dev environment
+  loadDevMessages();
+  loadErrorMessages();
+}
+const VITE_GRAPHQL_URL: string = import.meta.env.VITE_GRAPHQL_URL as string;
+const httpLink = new HttpLink({
+  uri: VITE_GRAPHQL_URL,
+  credentials: "include",
+});
+
+export const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 import { AuthProvider } from "./Auth.tsx";
+import { ApolloProvider } from "@apollo/client";
+import {client} from "../graphql/apollo-client.ts"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ApolloProvider client={client} >
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
