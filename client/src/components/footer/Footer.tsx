2dd392ef832ee0dcd197748e870237ff203a487e
johnmuthua <caveomuthama@gmail.com>
2024-06-06 11:24:41 +0300

    Add year

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <div className="bottom-0 flex justify-center py-4 bg-slate-300">
       <p className="text-center text-gray-600">
-        Copyright &copy; 2021 Reward Systems. All rights reserved.
+        Copyright &copy; {year} Reward Systems. All rights reserved.
       </p>
     </div>
   );
