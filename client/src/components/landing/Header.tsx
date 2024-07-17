c47b17ca0eecddb65dfcfb70685edeeabe225b19
johnmuthua <caveomuthama@gmail.com>
2024-07-17 09:57:18 +0300

    Add diffrent headers for small and medium

--- client/src/components/landing/Header.tsx
+++ client/src/components/landing/Header.tsx
@@ -1,11 +1,14 @@
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between">
+      <div className="flex justify-between px-4 py-6">
         <div>
           <h1>Reward System</h1>
         </div>
-        <div className="flex justify-between gap-2">
+        <div className="md:hidden">
+          <h1>x</h1>
+        </div>
+        <div className="md:flex justify-between gap-2 hidden">
           <div>Features</div>
           <div>About Us</div>
           <div>Contact Us</div>
