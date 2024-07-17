2a5f5af728d56e8d1e3369c452b68803875d5f97
johnmuthua <caveomuthama@gmail.com>
2024-07-17 09:54:42 +0300

    Add landing page

---
+++ client/src/components/landing/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>
+          <h1>Reward System</h1>
+        </div>
+        <div className="flex justify-between gap-2">
+          <div>Features</div>
+          <div>About Us</div>
+          <div>Contact Us</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,13 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import CreatePost from "./pages/CreatePost";
 import RootLayout from "./RootLayout";
 import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
   {
     element: <RootLayout />,
     children: [
       {
-        path: "/",
+        path: "/dashboard",
         element: <HomePage />,
       },
       {
