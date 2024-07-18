762dc872f770270ffc09ed4509fc34832b5d9fa2
johnmuthua <caveomuthama@gmail.com>
2024-07-18 18:12:48 +0300

    add title component

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -3,12 +3,18 @@ import { Button } from "../ui/button";
 
 const Post = () => {
   return (
-    <div >
+    <div>
       <div className="flex justify-between py-4">
-      <h1 className="uppercase text-3xl font-bold mb-4">Create a Post</h1>
-      <Button className="text-white bg-gray-700 hover:border-none hover:bg-green-600 hover:text-white">
+        <h1 className="uppercase text-3xl font-bold mb-4">Create a Post</h1>
+        <Button className="text-white bg-gray-700 hover:border-none hover:bg-green-600 hover:text-white">
           Publish
         </Button>
+      </div>
+      <div className="flex w-full">
+        <input
+          placeholder="Enter Post Title"
+          className="px-4 py-2 text-2xl font-bold border-2 border-gray-400 w-full my-4"
+        />
       </div>
       <EditorComponent />
       <div className="flex justify-between items-center pt-4 ">
