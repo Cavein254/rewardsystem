4192531169122172fdda758c06d64c5ffbbb6b17
johnmuthua <caveomuthama@gmail.com>
2024-07-18 20:08:05 +0300

    Add contact us

--- client/src/components/landing/Feature.tsx
+++ client/src/components/landing/Feature.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const ContactUs = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Contact Us Page</h1>
+    </div>
+  );
 };
 
-export default Feature;
+export default ContactUs;
