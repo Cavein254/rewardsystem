dcc121a3d079a4a6c0675b6a2dd65c2f8267fb36
johnmuthua <caveomuthama@gmail.com>
2024-07-24 11:15:51 +0300

    [chore] clean up

--- server/src/schema/resolvers/Query/getPostComments.ts
+++ server/src/schema/resolvers/Query/getPostComments.ts
@@ -2,6 +2,7 @@ import type { QueryResolvers } from "./../../types.generated";
 export const getPostComments: NonNullable<
   QueryResolvers["getPostComments"]
 > = async (_parent, arg, ctx) => {
+  console.log("calling commnets");
   const { prisma } = ctx;
   const { postId } = arg;
   const postComments = await prisma.comment.findMany({
